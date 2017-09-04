b2d8f65abe2aea5b728cf6684a166bffdef3fa35
jgodwinWX <jasonwgodwin@gmail.com>
2017-09-04 17:15:48 -0500

    Updated block header and disclaimer.

--- crosswind_tool.py
+++ crosswind_tool.py
@@ -3,10 +3,17 @@
 
 This tool will compute cross-wind components for various runway configurations. User inputs wind
 speed (in units), direction (degrees on the meteorological compass), and airport (ICAO code). The
-runway information is read in from a CSV file containing the runway names (i.e. "18/36L").
+runway information is read in from a CSV file containing the runway names (i.e. "18/36L"). The
+CSV file was provided courtesy OurAirports (http://ourairports.com/data/).
+
+Disclaimer: This code is provided "as is" with no warranty of any kind. The information provided
+by this tool should be considered unofficial and informal, and should not be used for any decisions
+that involve the protection of life or property.
 
 Version history:
     1.0: Initial build.
+    2.0: Now uses true north runway headings.
+    2.1: Added disclaimer and updated block header.
 
 '''
 # import modules
@@ -17,7 +24,7 @@ import pandas
 __author__ = "Jason W. Godwin"
 __copyright__ = "Public Domain"
 __license__ = "GPL"
-__version__ = "1.0"
+__version__ = "2.1"
 __maintainer__ = "Jason W. Godwin"
 __email__ = "jasonwgodwin@gmail.com"
 __status__ = "Production"
