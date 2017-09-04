c27746190d5fe1402e4d780eae196e2d6e64e2c6
jgodwinWX <jasonwgodwin@gmail.com>
2017-09-04 17:09:04 -0500

    Now computes crosswind to true north.

--- crosswind_tool.py
+++ crosswind_tool.py
@@ -30,6 +30,7 @@ airport = raw_input("Airport (ICAO code): ")
 # import runway information
 df = pandas.read_csv('airports/runways.csv')    # import the CSV as a dataframe
 df_airport = df[df['ID']==airport]              # reduce the dataframe to the relevant airport
+headings = np.array(df_airport['Heading'])      # runway headings (true north)
 
 # get runway names
 runways = []
@@ -38,14 +39,9 @@ for i,j in zip(df_airport['LowerID'],df_airport['UpperID']):
         i = "0" + i
     runways.append(i + "/" + j)
 
-# determine runway directions
-rnwy = np.zeros(len(runways))
-for i in range(len(runways)):
-    rnwy[i] = float(runways[i][0:2]) * 10.0
-
 # compute crosswind component on each runway and print the output
 print "Airport: %s" % airport
 print "Runway\tCross-wind component"
-for j in range(len(rnwy)):
-    xwind = windspd * abs(math.sin(math.radians(rnwy[j]-winddir)))
-    print "%s\t%.0f" % (runways[j],xwind)
+for j in range(len(headings)):
+    xwind = windspd * abs(math.sin(math.radians(headings[j]-winddir)))
+    print "%s\t%.2f" % (runways[j],xwind)
